ddc87a7ddc0f9e6194e547c455807e5d16170406
Alistair Bell <alistair@bellsonline.com>
2025-12-03 18:54:23 -0500

    and part 2!

--- day2.py
+++ day2.py
@@ -14,7 +14,7 @@ class Day2(adventday.AdventDay):
             self.input.append((int(splitrange[0]), len(splitrange[0]), int(splitrange[1]), len(splitrange[1])))
         
     def part_one(self):
-        invalids = 0
+        invalids = []
         for (low, lowlen, high, highlen) in self.input:
             # You can't have repeated patterns if the number of digits is odd
             if lowlen % 2 == 1:
@@ -46,9 +46,56 @@ class Day2(adventday.AdventDay):
                     thisval = val * (1 + (10**(lowlen//2)))
                     these_invalids.append(thisval)
             if self.args.verbose:
-                print(f'From {low} to {high} the invalid IDs are: {these_invalids} summing to {sum(these_invalids)}')
-            invalids += sum(these_invalids)
+                print(f'From {low} to {high} the invalid IDs are: {these_invalids} summing to ')
+            invalids += these_invalids
+        return sum(set(invalids))
+
+    def repeat_number(self, number, numberlen, split):
+        answer = 0
+        for i in range(split):
+            answer += number * (10**(numberlen*i))
+        return answer
+
+    def find_split_invalids(self, low, high, numberlen, split):
+        # We know here that we can split this number into this many pieces. So let's do it.
+        these_invalids = []
+        smallerlen = numberlen // split
+        halfdigitlow = low // (10**(smallerlen * (split-1)))
+        halfdigithigh = high // (10**(smallerlen * (split-1)))
+        firstrepeat = self.repeat_number(halfdigitlow, smallerlen, split)
+        if firstrepeat >= low and firstrepeat <= high:
+            these_invalids.append(firstrepeat)
+        lastrepeat = self.repeat_number(halfdigithigh, smallerlen, split)
+        if lastrepeat <= high and lastrepeat >= low and lastrepeat != firstrepeat:
+            these_invalids.append(lastrepeat)
+        # And all the numbers in between.
+        if halfdigitlow != halfdigithigh:
+            # Total should be 1/2n(n+1) * 1001 + lowval, roughly speaking
+            for val in range(halfdigitlow+1, halfdigithigh):
+                thisval = self.repeat_number(val, smallerlen, split)
+                these_invalids.append(thisval)
+        if self.args.verbose:
+            print(f'From {low} to {high} split by {split} the invalid IDs are: {these_invalids}')
+        return these_invalids
+
+
+    def find_invalids(self, low, high, numberlen):
+        # How many pieces can we break a number into?
+        invalids = []
+        for split in range(2, numberlen+1):
+            if numberlen % split == 0:
+                invalids += self.find_split_invalids(low, high, numberlen, split)
         return invalids
 
     def part_two(self):
-        return 0
+        invalids = []
+        for (low, lowlen, high, highlen) in self.input:
+            # First break up pieces of different lengths.
+            if lowlen != highlen:
+                assert highlen == lowlen + 1
+                invalids += self.find_invalids(low, 10**(highlen-1)-1, lowlen)
+                invalids += self.find_invalids(10**(highlen-1), high, highlen)
+            else:
+                invalids += self.find_invalids(low, high, highlen)
+
+        return sum(set(invalids))

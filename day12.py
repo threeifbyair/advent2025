038d193898c5abcb765fb613f2dbab4d174f3be3
Alistair Bell <alistair@bellsonline.com>
2025-12-08 22:49:34 -0500

    Input for day 6 and skeletons for all days!

---
+++ day12.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import re
+import sys, os
+
+import adventday
+
+class Day12(adventday.AdventDay):
+    def analyze_lines(self):
+        for line in self.lines:
+            pass
+
+    def part_one(self):
+        return 0
+
+    def part_two(self):
+        return 0

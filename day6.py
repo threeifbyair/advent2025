3da0e9a5c13f79f6db45f582b96b7e9638c93400
Alistair Bell <alistair@bellsonline.com>
2025-12-08 22:59:42 -0500

    Day 6 part 1.

--- day6.py
+++ day6.py
@@ -7,11 +7,27 @@ import adventday
 
 class Day6(adventday.AdventDay):
     def analyze_lines(self):
+        self.values = []
         for line in self.lines:
-            pass
+            if '*' in line or '+' in line:
+                self.operations = [x.strip() for x in line.split()]
+            else:
+                self.values.append([int(x) for x in line.strip().split()])
 
     def part_one(self):
-        return 0
+        total = 0
+        for pos, op in enumerate(self.operations):
+            if op == '*':
+                thistotal = 1
+                for value in self.values:
+                    thistotal *= value[pos]
+                total += thistotal
+            elif op == '+':
+                thistotal = 0
+                for value in self.values:
+                    thistotal += value[pos]
+                total += thistotal
+        return total
 
     def part_two(self):
         return 0

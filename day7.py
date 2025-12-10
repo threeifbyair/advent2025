a101619da1ae29fea8eeca86b2f71c30fd2a2463
Alistair Bell <alistair@bellsonline.com>
2025-12-09 21:29:24 -0500

    Day 7 part two!

--- day7.py
+++ day7.py
@@ -25,35 +25,20 @@ class Day7(adventday.AdventDay):
         return splits             
 
     def part_two(self):
-        # We need to re-interpret the lines...
-        spacelist = []
-        for line in self.lines[:-1]:
-            thesespaces = set()
-            for pos, ch in enumerate(line):
-                if ch == ' ':
-                    thesespaces.add(pos)
-            spacelist.append(thesespaces)
-        commonspaces = set.intersection(*spacelist)
-        breaks = [-1] + sorted(list(commonspaces)) + [max(len(x) for x in self.lines[:-1])]
-        # Now re-parse the values based on these common spaces
-        total = 0
-        for i in range(len(breaks)-1):
-            values = [0] * (breaks[i+1]-breaks[i]-1)
-            for j in range(breaks[i]+1, breaks[i+1]):
-                for k in self.lines[:-1]:
-                    if j < len(k) and k[j] != ' ':
-                        values[j-breaks[i]-1] *= 10
-                        values[j-breaks[i]-1] += int(k[j])
-                if j < len(self.lines[-1]) and self.lines[-1][j] in ('*', '+'):
-                    operation = self.lines[-1][j]
-            if operation == '*':
-                thistotal = 1
-                for value in values:
-                    thistotal *= value
-                total += thistotal
-            elif operation == '+':
-                thistotal = 0
-                for value in values:
-                    thistotal += value
-                total += thistotal
-        return total
+        tachyons = {self.values[1].index('S'): 1}
+        for line in self.values[2:]:
+            new_tachyons = {}
+            for pos in tachyons:
+                if line[pos] == '^':
+                    if pos-1 not in new_tachyons:
+                        new_tachyons[pos-1] = 0
+                    new_tachyons[pos-1] += tachyons[pos]
+                    if pos+1 not in new_tachyons:
+                        new_tachyons[pos+1] = 0
+                    new_tachyons[pos+1] += tachyons[pos]
+                else:
+                    if pos not in new_tachyons:
+                        new_tachyons[pos] = 0
+                    new_tachyons[pos] += tachyons[pos]
+            tachyons = new_tachyons
+        return sum(tachyons.values())

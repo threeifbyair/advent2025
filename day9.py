9532a2449edb22a6f9716f744edd19961181d6a5
Alistair Bell <alistair@bellsonline.com>
2025-12-12 18:40:26 -0500

    Day 9 part 1 -- and the files from day 8.

--- day9.py
+++ day9.py
@@ -7,11 +7,28 @@ import adventday
 
 class Day9(adventday.AdventDay):
     def analyze_lines(self):
+        self.num_closest = self.args.argint if self.args.argint else 1000
+        self.points = []
         for line in self.lines:
-            pass
+            self.points.append([int(x) for x in line.strip().split(',')])
+
+    def process_points(self, is_part_two=False):
+        self.areas = {}
+        for i, point in enumerate(self.points):
+            for j, otherpoint in enumerate(self.points[i+1:], start=i+1):
+                area = (abs(otherpoint[0]-point[0]) + 1) * (abs(otherpoint[1]-point[1]) + 1)
+                if self.args.verbose: 
+                    print(f'Area between point {i} {point} and point {j} {otherpoint} is {area}')
+                if area not in self.areas:
+                    self.areas[area] = []
+                self.areas[area].append( (i, j) )
+
+        sorted_areas = sorted(self.areas.keys())
+        return sorted_areas[-1]
 
     def part_one(self):
-        return 0
+        return self.process_points(is_part_two=False)
 
     def part_two(self):
-        return 0
+        return self.process_points(is_part_two=True)
+
